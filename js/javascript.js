38584cc009c4b8cdcdf05d2ae072057ec7ce2149
Gustavo Souza <36318691+gusouza97@users.noreply.github.com>
2022-04-26 17:01:52 -0300

    update javavscript.js

--- js/javascript.js
+++ js/javascript.js
@@ -5,6 +5,11 @@
     let initialize = false
     let orderRound = new Array()
     let orderPlayer = new Array()
+    let timer;
+    let check;
+    let piscou;
+    let loadLoop;
+    let recebeClique = false;
     let score = 0
     let gameOver;
     // 0 -> Green
@@ -22,42 +27,153 @@
 
     // LISTENERS
     btnPlay.addEventListener("click", () => {
-        start();
+        Start();
+    })
+
+    btnGreen.addEventListener("click", () => {
+        ReceberDados(0)
+    })
+
+    btnBlue.addEventListener("click", () => {
+        ReceberDados(3)
+    })
+
+    btnYellow.addEventListener("click", () => {
+        ReceberDados(2)
+    })
+
+    btnRed.addEventListener("click", () => {
+        ReceberDados(1)
     })
 
     // FUNCTIONS
     // Funcao para iniciar o game
-    function start() {
+    function Start() {
         initialize = true
         gameOver = false
+        timer = 1000;
         gamePanel.style.display = "flex"
         btnPlay.style.display = "none"
 
-        while(gameOver == false){
-            
-            orderRound.push(GenerateNumber());
-            PiscarLuzes(orderRound);
-            
-            gameOver = true;
+        // Gerando Sequencia da Rodada
+        orderRound.push(GenerateNumber());
+        orderRound.push(GenerateNumber());
+        orderRound.push(GenerateNumber());
+        orderRound.push(GenerateNumber());
+
+        for (let i = 0; i < orderRound.length; i++) {
+            check = true;
+            Counter(orderRound[i])
         }
+
+
+        // Ativando o recebimento dos dados
+        if (recebeClique != true) {
+
+        }
+
+        if (orderRound.length == orderPlayer.length) {
+            recebeClique = false;
+            ComparaResposta();
+        }
+
     }
 
     // Funcao para gerar numero da rodada
-    function GenerateNumber(){
+    function GenerateNumber() {
         let value = Math.floor(Math.random() * 4)
         return value
     }
 
-    function PiscarLuzes(orderRound){
-        for(key in orderRound){
-            
+    // Funcao para piscar as luzes
+    function PiscarLuzes(valor) {
+        switch (valor) {
+            case 0:
+                btnGreen.style.opacity = "0.3";
+                check = false;
+                setTimeout(() => {
+                    piscou = piscou + 1;
+                    btnGreen.style.opacity = "1";
+                    check = true;
+                }, timer)
+
+                break;
+
+            case 1:
+                btnRed.style.opacity = "0.3";
+                check = false;
+                setTimeout(() => {
+                    piscou = piscou + 1;
+                    btnRed.style.opacity = "1";
+                    check = true;
+                }, timer)
+                break;
+
+            case 2:
+                btnYellow.style.opacity = "0.3";
+                check = false;
+                setTimeout(() => {
+                    piscou = piscou + 1;
+                    btnYellow.style.opacity = "1";
+                    check = true;
+                }, timer)
+                break;
+
+            case 3:
+                btnBlue.style.opacity = "0.3";
+                check = false;
+                setTimeout(() => {
+                    piscou = piscou + 1;
+                    btnBlue.style.opacity = "1";
+                    check = true;
+                }, timer)
+                break;
+        }
+        recebeClique = false;
+    }
+
+
+
+    // Recebendo Dados
+    function ReceberDados(id) {
+        if (recebeClique == true) {
+            orderPlayer.push(id)
+            console.log(orderPlayer)
         }
     }
 
-    function CreateTimer(){
-        setTimeout(() => {
-            timer
-        })
+
+    // Comparando as respostas
+    function ComparaResposta() {
+        for (let i = 0; i < orderRound.length; i++) {
+            if (orderRound[i] != orderPlayer[i]) {
+                gameOver = true;
+                console.log("Game Over")
+            }
+        }
     }
+
+    // Funcao que auxilia no piscar das luzues
+    function Counter(valor) {
+
+        console.log(valor)
+
+        setTimeout(() => {
+            if(check == true){
+                console.log(valor)
+                PiscarLuzes(valor);
+            }
+        }, timer)
+        
+        
+
+        //Bloqueando o Timeout
+        if (piscou == orderRound.length - 1) {
+            return;
+        }
+
+        setTimeout(Counter, 1000);
+    }
+
 
 })(window, document);
